26586666abd0fbc0b1ec8865bc676fe95aee2ff4
Wasique Sheikh <wasique.sheikh@devprovider.com>
2024-08-06 12:24:46 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-push-notification-app',
+  webDir: 'www'
+};
+
+export default config;
